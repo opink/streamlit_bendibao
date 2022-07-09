858bce908a86c9370cdd614d549123a33eacb370
opink <opink@qq.com>
2022-07-09 17:03:24 +0800

    Update streamlit_bendibao.py

--- streamlit_bendibao.py
+++ streamlit_bendibao.py
@@ -19,41 +19,38 @@ class BendibaoFXDQ:
         self.headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/103.0.5060.114 Safari/537.36 Edg/103.0.1264.49'}
         self.zhixiashi = ['北京市','上海市','天津市','重庆市']
 
-    def get_list(self, path:str ='today') -> pd.DataFrame:
+    def get_list(self, path:str ='today') -> pd.DataFrame, timestrs:
         response = requests.get(self.base_url,headers=self.headers)
-        print(response)
+        # print(response) # 本地调试响应用
         jiexi = etree.HTML(response.text)
         got_time = jiexi.xpath('//div[@class="border-title"]//p[@class="time"]/text()')
-        print(got_time)
-        xiangmus = jiexi.xpath('//div[@class="info"]/div/p/text()')
-        print(xiangmus)
+        # print(got_time) # 本地调试用
+        # xiangmus = jiexi.xpath('//div[@class="info"]/div/p/text()') # 本地调试用
+        # print(xiangmus) 
         xiangmus = ['height-title','middle-title','tiaogao-title','tiaozhong-title','tiaodi-title']
         linshi_list = []
         for xiangmu in xiangmus:
             provinces = jiexi.xpath(f'//p[@class="{xiangmu}"]/..//div[@class="shi flex-between"]')
-            print(len(provinces))
+            # print(len(provinces)) # 调试用
             if len(provinces):
                 details = jiexi.xpath(f'//p[@class="{xiangmu}"]/..//ul[@class="info-detail"]')
-                assert len(details) == len(provinces)
+                assert len(details) == len(provinces) # 网页xpath两者是一一对应的
                 for province, sites in zip(provinces, details):
-                    if province[0][0][0].text not in self.zhixiashi:
+                    if province[0][0][0].text not in self.zhixiashi: # span[0]元素的文本是省份或者直辖市
                         prov, city = province[0][0][0].text, province[0][0][1].text
                     else: 
                         prov, city = province[0][0][0].text, province[0][0][0].text
                     for site in sites:
-                        linshi_list.append([xiangmu, prov, city, site[1].text])
+                        linshi_list.append([xiangmu, prov, city, site[1].text]) # 组装好一个数据
         df = pd.DataFrame(linshi_list,columns=['项目','省','市','点位'])
-        df['项目'] = df['项目'].replace({'height-title':'高风险地区',
-                                        'middle-title':'中风险地区',
-                                        'tiaogao-title':'今日调高',
-                                        'tiaozhong-title':'今日调中',
-                                        'tiaodi-title':'今日调低'})
+        df['项目'] = df['项目'].replace({'height-title':'高风险地区','middle-title':'中风险地区','tiaogao-title':'今日调高',
+                                        'tiaozhong-title':'今日调中','tiaodi-title':'今日调低'}
+                                        )
         return df, got_time
 
-
+# @st.cache加在哪里啊?没整明白啊
 def convert_df(df):
-    # IMPORTANT: Cache the conversion to prevent computation on every rerun
-    return df.to_csv().encode('gbk') # raw python 不支持ANSI
+    return df.to_csv().encode('gbk') # raw python 不支持ANSI么?
 
 st.title('通过本地宝获取全国疫情中高风险地区名单的CSV表格')
 st.write('实时更新数据来源 ->  http://m.bendibao.com/news/gelizhengce/fengxianmingdan.php')
